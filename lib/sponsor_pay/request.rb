03cd645382c7e74657496eb9bcd1335146654551
Victor Pereira <vpereira@web.de>
2012-07-30 23:08:09 +0200

    tidy up

--- lib/sponsor_pay/request.rb
+++ lib/sponsor_pay/request.rb
@@ -1,8 +1,9 @@
 require 'digest/sha1'
 
 module SponsorPay
+  API_KEY = "b07a12df7d52e6c118e5d47d3f9e60135b109a1f"
   class Request 
-    attr_reader :uri,:query_string, :params
+    attr_reader :uri,:query_string, :params, :hashkey
     def initialize(params = {})
       @params = {
         appid: "157",
@@ -14,6 +15,17 @@ module SponsorPay
         uid: "player1",
         timestamp: Time.now.to_i,
       }.merge(params)
+    end
+    def query_string
+      @query_string ||=  params.keys.sort.collect { |p|
+        "#{p}=#{params[p.to_sym]}"
+      }.join("&") 
+    end
+    def hashkey
+      @hashkey ||= Digest::SHA1.hexdigest query_string + "&#{API_KEY}"
+    end
+    def uri
+      @uri ||= "http://api.sponsorpay.com/feed/v1/offers.json?#{query_string}&hashkey=#{hashkey}"
     end
   end
 end
--- spec/sponsor_pay/request_spec.rb
+++ spec/sponsor_pay/request_spec.rb
@@ -1,4 +1,5 @@
-require 'spec_helper'
+require_relative '../spec_helper'
+require 'uri'
 
 describe SponsorPay::Request do
   context "default values" do
@@ -8,6 +9,13 @@ describe SponsorPay::Request do
     it { @request.should_not be_nil }
     it { @request.params.keys.should_not be_empty }
     it { @request.params.keys.should == [:appid,:format,:device_id,:locale,:ip,:offer_types,:uid,:timestamp] }
+    it { @request.query_string.should be_a String }
+    it { @request.hashkey.should match /\b([a-f0-9]{40})\b/ }
+    
+    describe ".uri" do
+      it { lambda { URI.parse(@request.uri) }.should_not raise_error }
+      it { URI.parse(@request.uri).should_not be_nil }
+    end
   end
   context "with specific uid" do
     before do
