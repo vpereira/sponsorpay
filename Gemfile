4e4717eae890e3a5d374a0becb29d5ceae4a2ea0
Victor Pereira <victor.pereira@adyton.net>
2012-07-31 11:58:24 +0200

    added views directory

--- Gemfile
+++ Gemfile
@@ -1,8 +1,11 @@
 source 'http://rubygems.org'
 gem 'goliath'
+gem 'tilt'
 gem 'rspec'
 gem 'simplecov'
 gem 'haml'
 gem 'em-http-request'
 gem 'json' #just for ruby 1.8.7
 gem "eventmachine"
+gem "active_support"
+
--- sponsorpay.rb
+++ sponsorpay.rb
@@ -6,18 +6,41 @@ require "rubygems"
 require "bundler/setup"
 require 'goliath'
 require 'em-synchrony/em-http'
+require 'active_support'
+require 'active_support/hash_with_indifferent_access'
 require 'json'
+require 'haml'
+require 'goliath/rack/templates'
 require File.join(File.dirname(__FILE__),"lib","sponsor_pay","request")
 
-class SponsorPayHandler < Goliath::API
+class ProcessRequest < Goliath::API
   use Goliath::Rack::Params
   use Goliath::Rack::JSONP
   use Goliath::Rack::Render 
 
-  
   def response(env)
-    params = {}
+    params = ActiveSupport::HashWithIndifferentAccess.new(env)[:params] rescue {}
     http = SponsorPay::Request.new(params).get
     [200, {'X-SponsorPay' => 'Proxy','Content-Type' => 'application/json'}, http.response]
   end
+
 end
+
+class RenderIndex < Goliath::API
+  include Goliath::Rack::Templates
+
+  def response(env)
+    [200,{},haml(:default)]
+  end
+end
+
+class SponsorPayHandler < Goliath::API
+  map "/", RenderIndex
+  #TODO check the param2 in the documentation
+  map "/:uid/:param2",ProcessRequest
+end
+
+runner = Goliath::Runner.new(ARGV, nil)
+runner.api = SponsorPayHandler.new
+runner.app = Goliath::Rack::Builder.build(SponsorPayHandler, runner.api)
+runner.run
