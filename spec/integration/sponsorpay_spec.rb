76c5753fd919077c565d9122ee0346b5f1b81024
Victor Pereira <vpereira@web.de>
2012-07-31 22:43:59 +0200

    tidy up and updated views following the specs

--- spec/integration/sponsorpay_spec.rb
+++ spec/integration/sponsorpay_spec.rb
@@ -4,9 +4,9 @@ describe SponsorPay do
 
   it "should get a 200 code" do
     with_api(RenderIndex,{:layout_engine=>:haml}) do
-	get_request(:path=>"/") do |c|
-	  c.response.should =~ %r{<h1>SEND FORM</h1>}
-	end
+	    get_request(:path=>"/") do |c|
+	      c.response.should =~ %r{<h1>SEND FORM</h1>}
+	    end
     end
   end
 end
